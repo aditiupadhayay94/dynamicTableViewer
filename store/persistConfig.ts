638af734b8b3d1f7ff6421c0e89a60e1b7991309
Aditi Upadhayay <upadhyayaditi94@gmail.com>
2025-11-05 01:56:00 +0530

    fix: import storage for redux-persist

--- store/persistConfig.ts
+++ store/persistConfig.ts
@@ -1,13 +1,25 @@
-
 import { PersistConfig } from "redux-persist";
+import type { WebStorage } from "redux-persist";
 
-// Only import storage in the browser
-let storage: any = undefined;
+// Create a dummy storage fallback for SSR builds
+const createNoopStorage = (): WebStorage => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
 
-if (typeof window !== "undefined") {
-  
-  storage = require("redux-persist/lib/storage").default;
-}
+const storage =
+  typeof window !== "undefined"
+    ? require("redux-persist/lib/storage").default
+    : createNoopStorage();
 
 const persistConfig: PersistConfig<any> = {
   key: "table",
