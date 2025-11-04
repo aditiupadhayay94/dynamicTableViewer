62e5e616eac096f0250973aecd103dfbbccc0be7
Aditi Upadhayay <upadhyayaditi94@gmail.com>
2025-11-05 01:40:24 +0530

    fix: import storage for redux-persist

--- store/persistConfig.ts
+++ store/persistConfig.ts
@@ -1,4 +1,15 @@
+
 import { PersistConfig } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+
+const persistConfig: PersistConfig<any> = {
+  key: 'table',
+  storage,
+  whitelist: ['columns'],
+};
+
+export default persistConfig;
+
 
 const persistConfig: PersistConfig<any> = {
   key: 'table',
